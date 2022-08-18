6f7d7b186c40749e12fa32e14aa7ab1c5cdf72e0
Fahad <92694740+iamfahad43@users.noreply.github.com>
2022-08-19 01:42:38 +0500

    Script to Fetch tags and write into exif file

--- MetaTags.py
+++ MetaTags.py
@@ -1,18 +1,16 @@
-
-# Get Images tags using API
-
 import os
 import io
 from google.cloud import vision
 from google.oauth2 import service_account
+import PIL.Image
+import PIL.ExifTags
 
 # here is the path where our REST APi is
-credentials = service_account.Credentials.from_service_account_file('/var/services/homes/dev2_img/Python_Script/apicredentials.json')
+credentials = service_account.Credentials.from_service_account_file('/mnt/d/Important_documents/apicredentials.json')
 
 client = vision.ImageAnnotatorClient(credentials=credentials)
 
-def detect_labels(ImageName, DirectoryPath):
-    ImagesToGetTags = DirectoryPath + "/" + str(ImageName)
+def detect_labels(ImagesToGetTags):
     print("detect_labels() was called")
     print(f"This is what was passed to detect_labels() - {ImagesToGetTags}\n")
     #client = vision.ImageAnnotatorClient(credentials=credentials)
@@ -24,21 +22,27 @@ def detect_labels(ImageName, DirectoryPath):
     labels = response.label_annotations
     print('Labels:')
     
-    FinalFileName = os.path.join(DirectoryPath, ImageName+".Exif")
-    with open(FinalFileName, 'a') as exiffile:
-        for label in labels:
-            LablDescription = label.description
-            LablScore = label.score 
-            print(f"{LablDescription}: ({round(LablScore * 100)}%)")
-            exiffile.write(f"{LablDescription}: {LablScore} ")
+    img = PIL.Image.open(ImagesToGetTags)
+    exif = {
+    PIL.ExifTags.TAGS[k]: v
+    for k, v in img._getexif().items()
+    if k in PIL.ExifTags.TAGS
+    }
+
+    LabelsList = []
+    for label in labels:
+        LablDescription = label.description
+        LablScore = label.score 
+        print(f"{LablDescription}: ({round(LablScore * 100)}%)")
+        LabelsList.append(LablDescription)
     
-        if response.error.message:
-            raise Exception(
-                '{}\nFor more info on error messages, check: '
-                'https://cloud.google.com/apis/design/errors'.format(
-                    response.error.message))
-        exiffile.close()
-        print("successfully wrote to exif file")
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+    LabelsStr = "\n".join(LabelsList)
+    exif["Labels"] = LabelsStr
 
 def FetchAllDirectories(dirname):
     subfolders= [f.path for f in os.scandir(dirname) if f.is_dir()]
@@ -51,18 +55,29 @@ def GetImageTags(folderpath):
 
     for images in os.listdir(folderpath):
         if images[-4:] == ".JPG" or images[-4:] == ".jpg":
-            OutPutTags = detect_labels(images, folderpath)
+            ImagesToGetTags = folderpath + "/" + str(images)
+            OutPutTags = detect_labels(ImagesToGetTags)
             infoList.append(OutPutTags)
             print("\n")
     return infoList
 
+
 if __name__== "__main__":
-    folderpath = input("Enter folder path:\n")
+    
+    ListSubfolders = [f.path for f in os.scandir("/mnt/d/Work/") if f.is_dir()]
+    for index, dr in enumerate(ListSubfolders):
+        print(index+1, dr)
+    UserChoice = int(input("\nPlease select User by Number:\n"))
+    
+    folderpath = ListSubfolders[UserChoice-1]
     OutPut = FetchAllDirectories(folderpath)
     
     print(f"\nList of subdirectories from given path are: {OutPut}\nTatal number of directories wea re working on is: {len(OutPut)}\n")
+    TotalImagesPassed = 0
     for index, filepaths in enumerate(OutPut):
         print(f"\nWorking with Images in path: {filepaths}\n")
         ListOut = GetImageTags(filepaths)
         print(f"Completed {index+1} directory \n")
+        TotalImagesPassed = TotalImagesPassed + len(ListOut)
     
+    print(f"Successfully passed {TotalImagesPassed} Images")
